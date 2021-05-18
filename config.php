344c7d28773326bfac8dc088128484fe7cba589d
Muhammad Sazzad Hossain <59094087+0xSazzad@users.noreply.github.com>
2021-05-18 13:58:27 +0600

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 class Connect extends PDO {
     public function __construct() {
-        parent::__construct("mysql:host=sql301.hstn.me;dbname=mseet_28415402_api", 'mseet_28415402', '98121298',
+        parent::__construct("mysql:host=localhost;dbname=api", 'root', '',
         array(PDO::MYSQL_ATTR_INIT_COMMANT => "SET NAMES utf8"));
         $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $this->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
